39c7f0c80fc103db09b1a6b1eb848c2033b8529a
crocokyle <me@kyle-dixon.com>
2024-12-13 16:59:32 -0800

    ⚗️ Add boilerplate vue code

---
+++ app/static/js/app.js
@@ -0,0 +1,6 @@
+// You can add additional Petite-Vue configurations here if needed
+import PetiteVue from "../../../.venv/Lib/site-packages/nicegui/static/vue.global";
+
+document.addEventListener("DOMContentLoaded", () => {
+    PetiteVue.createApp({}).mount();
+});
