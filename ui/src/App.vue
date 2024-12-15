ce64e9802955ab9b28750e099690c6a2b9251e46
Andrew <andrewsmadden@gmail.com>
2024-12-15 01:46:35 -0600

    Some initial changes to get commands listed.

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,85 +1,34 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import type {command} from "@/components/CommandList.vue";
+import CommandList from "@/components/CommandList.vue";
+
+var commands: command[] = [
+  {title: "echo", command: "echo", args: ['test'], workingDir: '~'},
+  {title: 'test', command: 'test', args: ['test'], workingDir: '~'},
+]
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <h1>Command Runner</h1>
+  <div class="content">
+    <CommandList :commands=commands></CommandList>
+  </div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+body {
+  margin: 0;
+  padding: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.content {
+  display: flex;
+  flex-direction: column;
+  width: 70%;
+  background: #001;
+  border-radius: 10px;
 }
 </style>
---
+++ ui/src/components/CommandList.vue
@@ -0,0 +1,53 @@
+<script setup lang="ts">
+export type command = {
+  title: string,
+  command: string,
+  workingDir: string,
+  args: string[],
+};
+
+defineProps<{
+  commands: command[]
+}>();
+</script>
+
+<template>
+  <div class="command-list">
+    <details class="command-item" v-for="command in commands">
+      <summary>
+        <span class="command-title">{{command.title}}</span>
+        <button class="command-run">Run</button>
+      </summary>
+      Filler Content
+    </details>
+  </div>
+</template>
+
+<style scoped>
+.command-list {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+}
+
+.command-item {
+  width: 100%;
+  display: flex;
+  justify-content: space-evenly;
+}
+
+.command-item summary {
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-evenly;
+}
+
+.command-title {
+  align-self: flex-start;
+}
+
+.command-run {
+  align-self: flex-end;
+}
+</style>
